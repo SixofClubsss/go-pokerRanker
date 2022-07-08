fa9254d67b747b919ede07cf1c0f6703faffdb39
SixofClubs <84689659+SixofClubsss@users.noreply.github.com>
2022-07-07 18:49:19 -0600

    Commit files
    Initial commit

---
+++ hands.go
@@ -0,0 +1,634 @@
+/*
+	dReam Tables Poker Hand Ranker
+	Originally written in C++ and used in dReam Tables Dero Poker Tables
+	https://dreamtables.net
+	Translated to go
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var totalHands int
+var p1HandRaw [5]int
+var p2HandRaw [5]int
+var p3HandRaw [5]int
+var p4HandRaw [5]int
+var p5HandRaw [5]int
+var p6HandRaw [5]int
+var arrSplit [2]int
+
+var pc1 [2]int
+var pc2 [2]int
+var pc3 [2]int
+var pc4 [2]int
+var pc5 [2]int
+var p1HighPair int
+var p2HighPair int
+var p3HighPair int
+var p4HighPair int
+var p5HighPair int
+var p6HighPair int
+var p1Rank int
+var p2Rank int
+var p3Rank int
+var p4Rank int
+var p5Rank int
+var p6Rank int
+var p1HighCardArr [5]int
+var p2HighCardArr [5]int
+var p3HighCardArr [5]int
+var p4HighCardArr [5]int
+var p5HighCardArr [5]int
+var p6HighCardArr [5]int
+
+func clearHands() { /// Clears hand arrays before new input
+	p1HandRaw = [5]int{0, 0, 0, 0, 0}
+	p2HandRaw = [5]int{0, 0, 0, 0, 0}
+	p3HandRaw = [5]int{0, 0, 0, 0, 0}
+	p4HandRaw = [5]int{0, 0, 0, 0, 0}
+	p5HandRaw = [5]int{0, 0, 0, 0, 0}
+	p6HandRaw = [5]int{0, 0, 0, 0, 0}
+}
+func getHands(totalHands int) { /// Gets card values for totalHands selected
+	p1Rank = 100
+	p1HighPair = 0
+	p2Rank = 100
+	p2HighPair = 0
+	p3Rank = 100
+	p3HighPair = 0
+	p4Rank = 100
+	p4HighPair = 0
+	p5Rank = 100
+	p5HighPair = 0
+	p6Rank = 100
+	p6HighPair = 0
+
+	switch totalHands {
+	case 2:
+		p1Rank = getHand1()
+		p2Rank = getHand2()
+	case 3:
+		p1Rank = getHand1()
+		p2Rank = getHand2()
+		p3Rank = getHand3()
+	case 4:
+		p1Rank = getHand1()
+		p2Rank = getHand2()
+		p3Rank = getHand3()
+		getHand4()
+	case 5:
+		p1Rank = getHand1()
+		p2Rank = getHand2()
+		p3Rank = getHand3()
+		p4Rank = getHand4()
+		p5Rank = getHand5()
+	case 6:
+		p1Rank = getHand1()
+		p2Rank = getHand2()
+		p3Rank = getHand3()
+		p4Rank = getHand4()
+		p5Rank = getHand5()
+		p6Rank = getHand6()
+	}
+
+}
+
+func getHand1() int { /// Splitting card value and suit for making individual hand, highcard value and rank
+	for {
+		fmt.Println("Input the card values for Player 1: ")
+
+		_, err := fmt.Scanln(&p1HandRaw[0], &p1HandRaw[1], &p1HandRaw[2], &p1HandRaw[3], &p1HandRaw[4])
+		if err != nil {
+			if p1HandRaw[4] == 0 { /// Did not input 5 cards
+				fmt.Println("Incorrect Format, Try Again")
+			}
+
+			if p1HandRaw[4] > 0 { /// Input had extra card, removed
+				fmt.Println("Extra input was removed")
+				var discard string
+				fmt.Scanln(&discard)
+			}
+
+		}
+
+		if p1HandRaw[0] > 0 && p1HandRaw[0] < 53 && p1HandRaw[1] > 0 && p1HandRaw[1] < 53 && p1HandRaw[2] > 0 &&
+			p1HandRaw[2] < 53 && p1HandRaw[3] > 0 && p1HandRaw[3] < 53 && p1HandRaw[4] > 0 && p1HandRaw[4] < 53 { /// Ensure correct input format
+			break
+		}
+	}
+
+	suitSplit(p1HandRaw[0])
+	pc1 = [2]int{arrSplit[0], arrSplit[1]}
+	p1HighCardArr[0] = arrSplit[0]
+
+	suitSplit(p1HandRaw[1])
+	pc2 = [2]int{arrSplit[0], arrSplit[1]}
+	p1HighCardArr[1] = arrSplit[0]
+
+	suitSplit(p1HandRaw[2])
+	pc3 = [2]int{arrSplit[0], arrSplit[1]}
+	p1HighCardArr[2] = arrSplit[0]
+
+	suitSplit(p1HandRaw[3])
+	pc4 = [2]int{arrSplit[0], arrSplit[1]}
+	p1HighCardArr[3] = arrSplit[0]
+
+	suitSplit(p1HandRaw[4])
+	pc5 = [2]int{arrSplit[0], arrSplit[1]}
+	p1HighCardArr[4] = arrSplit[0]
+
+	Rank := makeHand()
+
+	if pc1[0] == 1 {
+		p1HighCardArr[0] = 14
+	}
+
+	if pc2[0] == 1 {
+		p1HighCardArr[1] = 14
+	}
+
+	if pc3[0] == 1 {
+		p1HighCardArr[2] = 14
+	}
+
+	if pc4[0] == 1 {
+		p1HighCardArr[3] = 14
+	}
+
+	if pc5[0] == 1 {
+		p1HighCardArr[4] = 14
+	}
+
+	for i := 0; i < 4; i++ {
+		if p1HighCardArr[i] == p1HighCardArr[i+1] {
+			if p1HighCardArr[i] > p1HighPair {
+				p1HighPair = p1HighCardArr[i]
+			}
+		}
+	}
+
+	return Rank
+}
+
+func getHand2() int {
+	for {
+		fmt.Println("Input the card values for Player 2: ")
+
+		_, err := fmt.Scanln(&p2HandRaw[0], &p2HandRaw[1], &p2HandRaw[2], &p2HandRaw[3], &p2HandRaw[4])
+		if err != nil {
+			if p2HandRaw[4] == 0 {
+				fmt.Println("Incorrect Format, Try Again")
+			}
+
+			if p2HandRaw[4] > 0 {
+				fmt.Println("Extra input was removed")
+				var discard string
+				fmt.Scanln(&discard)
+			}
+
+		}
+
+		if p2HandRaw[0] > 0 && p2HandRaw[0] < 53 && p2HandRaw[1] > 0 && p2HandRaw[1] < 53 && p2HandRaw[2] > 0 &&
+			p2HandRaw[2] < 53 && p2HandRaw[3] > 0 && p2HandRaw[3] < 53 && p2HandRaw[4] > 0 && p2HandRaw[4] < 53 {
+			break
+		}
+	}
+
+	suitSplit(p2HandRaw[0])
+	pc1 = [2]int{arrSplit[0], arrSplit[1]}
+	p2HighCardArr[0] = arrSplit[0]
+
+	suitSplit(p2HandRaw[1])
+	pc2 = [2]int{arrSplit[0], arrSplit[1]}
+	p2HighCardArr[1] = arrSplit[0]
+
+	suitSplit(p2HandRaw[2])
+	pc3 = [2]int{arrSplit[0], arrSplit[1]}
+	p2HighCardArr[2] = arrSplit[0]
+
+	suitSplit(p2HandRaw[3])
+	pc4 = [2]int{arrSplit[0], arrSplit[1]}
+	p2HighCardArr[3] = arrSplit[0]
+
+	suitSplit(p2HandRaw[4])
+	pc5 = [2]int{arrSplit[0], arrSplit[1]}
+	p2HighCardArr[4] = arrSplit[0]
+
+	Rank := makeHand()
+
+	if pc1[0] == 1 {
+		p2HighCardArr[0] = 14
+	}
+
+	if pc2[0] == 1 {
+		p2HighCardArr[1] = 14
+	}
+
+	if pc3[0] == 1 {
+		p2HighCardArr[2] = 14
+	}
+
+	if pc4[0] == 1 {
+		p2HighCardArr[3] = 14
+	}
+
+	if pc5[0] == 1 {
+		p2HighCardArr[4] = 14
+	}
+
+	for i := 0; i < 4; i++ {
+		if p2HighCardArr[i] == p2HighCardArr[i+1] {
+			if p2HighCardArr[i] > p2HighPair {
+				p2HighPair = p2HighCardArr[i]
+			}
+		}
+	}
+
+	return Rank
+}
+
+func getHand3() int {
+	for {
+		fmt.Println("Input the card values for Player 3: ")
+
+		_, err := fmt.Scanln(&p3HandRaw[0], &p3HandRaw[1], &p3HandRaw[2], &p3HandRaw[3], &p3HandRaw[4])
+		if err != nil {
+			if p3HandRaw[4] == 0 {
+				fmt.Println("Incorrect Format, Try Again")
+			}
+
+			if p3HandRaw[4] > 0 {
+				fmt.Println("Extra input was removed")
+				var discard string
+				fmt.Scanln(&discard)
+			}
+
+		}
+
+		if p3HandRaw[0] > 0 && p3HandRaw[0] < 53 && p3HandRaw[1] > 0 && p3HandRaw[1] < 53 && p3HandRaw[2] > 0 &&
+			p3HandRaw[2] < 53 && p3HandRaw[3] > 0 && p3HandRaw[3] < 53 && p3HandRaw[4] > 0 && p3HandRaw[4] < 53 {
+			break
+		}
+	}
+
+	suitSplit(p3HandRaw[0])
+	pc1 = [2]int{arrSplit[0], arrSplit[1]}
+	p3HighCardArr[0] = arrSplit[0]
+
+	suitSplit(p3HandRaw[1])
+	pc2 = [2]int{arrSplit[0], arrSplit[1]}
+	p3HighCardArr[1] = arrSplit[0]
+
+	suitSplit(p3HandRaw[2])
+	pc3 = [2]int{arrSplit[0], arrSplit[1]}
+	p3HighCardArr[2] = arrSplit[0]
+
+	suitSplit(p3HandRaw[3])
+	pc4 = [2]int{arrSplit[0], arrSplit[1]}
+	p3HighCardArr[3] = arrSplit[0]
+
+	suitSplit(p3HandRaw[4])
+	pc5 = [2]int{arrSplit[0], arrSplit[1]}
+	p3HighCardArr[4] = arrSplit[0]
+
+	Rank := makeHand()
+
+	if pc1[0] == 1 {
+		p3HighCardArr[0] = 14
+	}
+
+	if pc2[0] == 1 {
+		p3HighCardArr[1] = 14
+	}
+
+	if pc3[0] == 1 {
+		p3HighCardArr[2] = 14
+	}
+
+	if pc4[0] == 1 {
+		p3HighCardArr[3] = 14
+	}
+
+	if pc5[0] == 1 {
+		p3HighCardArr[4] = 14
+	}
+
+	for i := 0; i < 4; i++ {
+		if p3HighCardArr[i] == p3HighCardArr[i+1] {
+			if p3HighCardArr[i] > p3HighPair {
+				p3HighPair = p3HighCardArr[i]
+			}
+		}
+	}
+
+	return Rank
+}
+
+func getHand4() int {
+	for {
+		fmt.Println("Input the card values for Player 4: ")
+
+		_, err := fmt.Scanln(&p4HandRaw[0], &p4HandRaw[1], &p4HandRaw[2], &p4HandRaw[3], &p4HandRaw[4])
+		if err != nil {
+			if p4HandRaw[4] == 0 {
+				fmt.Println("Incorrect Format, Try Again")
+			}
+
+			if p4HandRaw[4] > 0 {
+				fmt.Println("Extra input was removed")
+				var discard string
+				fmt.Scanln(&discard)
+			}
+
+		}
+
+		if p4HandRaw[0] > 0 && p4HandRaw[0] < 53 && p4HandRaw[1] > 0 && p4HandRaw[1] < 53 && p4HandRaw[2] > 0 &&
+			p4HandRaw[2] < 53 && p4HandRaw[3] > 0 && p4HandRaw[3] < 53 && p4HandRaw[4] > 0 && p4HandRaw[4] < 53 {
+			break
+		}
+	}
+
+	suitSplit(p4HandRaw[0])
+	pc1 = [2]int{arrSplit[0], arrSplit[1]}
+	p4HighCardArr[0] = arrSplit[0]
+
+	suitSplit(p4HandRaw[1])
+	pc2 = [2]int{arrSplit[0], arrSplit[1]}
+	p4HighCardArr[1] = arrSplit[0]
+
+	suitSplit(p4HandRaw[2])
+	pc3 = [2]int{arrSplit[0], arrSplit[1]}
+	p4HighCardArr[2] = arrSplit[0]
+
+	suitSplit(p4HandRaw[3])
+	pc4 = [2]int{arrSplit[0], arrSplit[1]}
+	p4HighCardArr[3] = arrSplit[0]
+
+	suitSplit(p4HandRaw[4])
+	pc5 = [2]int{arrSplit[0], arrSplit[1]}
+	p4HighCardArr[4] = arrSplit[0]
+
+	Rank := makeHand()
+
+	if pc1[0] == 1 {
+		p4HighCardArr[0] = 14
+	}
+
+	if pc2[0] == 1 {
+		p4HighCardArr[1] = 14
+	}
+
+	if pc3[0] == 1 {
+		p4HighCardArr[2] = 14
+	}
+
+	if pc4[0] == 1 {
+		p4HighCardArr[3] = 14
+	}
+
+	if pc5[0] == 1 {
+		p4HighCardArr[4] = 14
+	}
+
+	for i := 0; i < 4; i++ {
+		if p4HighCardArr[i] == p4HighCardArr[i+1] {
+			if p4HighCardArr[i] > p4HighPair {
+				p4HighPair = p4HighCardArr[i]
+			}
+		}
+	}
+
+	return Rank
+}
+
+func getHand5() int {
+	for {
+		fmt.Println("Input the card values for Player 5: ")
+
+		_, err := fmt.Scanln(&p5HandRaw[0], &p5HandRaw[1], &p5HandRaw[2], &p5HandRaw[3], &p5HandRaw[4])
+		if err != nil {
+			if p5HandRaw[4] == 0 {
+				fmt.Println("Incorrect Format, Try Again")
+			}
+
+			if p5HandRaw[4] > 0 {
+				fmt.Println("Extra input was removed")
+				var discard string
+				fmt.Scanln(&discard)
+			}
+
+		}
+
+		if p5HandRaw[0] > 0 && p5HandRaw[0] < 53 && p5HandRaw[1] > 0 && p5HandRaw[1] < 53 && p5HandRaw[2] > 0 &&
+			p5HandRaw[2] < 53 && p5HandRaw[3] > 0 && p5HandRaw[3] < 53 && p5HandRaw[4] > 0 && p5HandRaw[4] < 53 {
+			break
+		}
+	}
+
+	suitSplit(p5HandRaw[0])
+	pc1 = [2]int{arrSplit[0], arrSplit[1]}
+	p5HighCardArr[0] = arrSplit[0]
+
+	suitSplit(p5HandRaw[1])
+	pc2 = [2]int{arrSplit[0], arrSplit[1]}
+	p5HighCardArr[1] = arrSplit[0]
+
+	suitSplit(p5HandRaw[2])
+	pc3 = [2]int{arrSplit[0], arrSplit[1]}
+	p5HighCardArr[2] = arrSplit[0]
+
+	suitSplit(p5HandRaw[3])
+	pc4 = [2]int{arrSplit[0], arrSplit[1]}
+	p5HighCardArr[3] = arrSplit[0]
+
+	suitSplit(p5HandRaw[4])
+	pc5 = [2]int{arrSplit[0], arrSplit[1]}
+	p5HighCardArr[4] = arrSplit[0]
+
+	Rank := makeHand()
+
+	if pc1[0] == 1 {
+		p5HighCardArr[0] = 14
+	}
+
+	if pc2[0] == 1 {
+		p5HighCardArr[1] = 14
+	}
+
+	if pc3[0] == 1 {
+		p5HighCardArr[2] = 14
+	}
+
+	if pc4[0] == 1 {
+		p5HighCardArr[3] = 14
+	}
+
+	if pc5[0] == 1 {
+		p5HighCardArr[4] = 14
+	}
+
+	for i := 0; i < 4; i++ {
+		if p5HighCardArr[i] == p5HighCardArr[i+1] {
+			if p5HighCardArr[i] > p5HighPair {
+				p5HighPair = p5HighCardArr[i]
+			}
+		}
+	}
+
+	return Rank
+}
+
+func getHand6() int {
+	for {
+		fmt.Println("Input the card values for Player 6: ")
+
+		_, err := fmt.Scanln(&p6HandRaw[0], &p6HandRaw[1], &p6HandRaw[2], &p6HandRaw[3], &p6HandRaw[4])
+		if err != nil {
+			if p6HandRaw[4] == 0 {
+				fmt.Println("Incorrect Format, Try Again")
+			}
+
+			if p6HandRaw[4] > 0 {
+				fmt.Println("Extra input was removed")
+				var discard string
+				fmt.Scanln(&discard)
+			}
+
+		}
+
+		if p6HandRaw[0] > 0 && p6HandRaw[0] < 53 && p6HandRaw[1] > 0 && p6HandRaw[1] < 53 && p6HandRaw[2] > 0 &&
+			p6HandRaw[2] < 53 && p6HandRaw[3] > 0 && p6HandRaw[3] < 53 && p6HandRaw[4] > 0 && p6HandRaw[4] < 53 {
+			break
+		}
+	}
+
+	suitSplit(p6HandRaw[0])
+	pc1 = [2]int{arrSplit[0], arrSplit[1]}
+	p6HighCardArr[0] = arrSplit[0]
+
+	suitSplit(p6HandRaw[1])
+	pc2 = [2]int{arrSplit[0], arrSplit[1]}
+	p6HighCardArr[1] = arrSplit[0]
+
+	suitSplit(p6HandRaw[2])
+	pc3 = [2]int{arrSplit[0], arrSplit[1]}
+	p6HighCardArr[2] = arrSplit[0]
+
+	suitSplit(p6HandRaw[3])
+	pc4 = [2]int{arrSplit[0], arrSplit[1]}
+	p6HighCardArr[3] = arrSplit[0]
+
+	suitSplit(p6HandRaw[4])
+	pc5 = [2]int{arrSplit[0], arrSplit[1]}
+	p6HighCardArr[4] = arrSplit[0]
+
+	Rank := makeHand()
+
+	if pc1[0] == 1 {
+		p6HighCardArr[0] = 14
+	}
+
+	if pc2[0] == 1 {
+		p6HighCardArr[1] = 14
+	}
+
+	if pc3[0] == 1 {
+		p6HighCardArr[2] = 14
+	}
+
+	if pc4[0] == 1 {
+		p6HighCardArr[3] = 14
+	}
+
+	if pc5[0] == 1 {
+		p6HighCardArr[4] = 14
+	}
+
+	for i := 0; i < 4; i++ {
+		if p6HighCardArr[i] == p6HighCardArr[i+1] {
+			if p6HighCardArr[i] > p6HighPair {
+				p6HighPair = p6HighCardArr[i]
+			}
+		}
+	}
+
+	return Rank
+}
+
+func makeHand() int { /// Determines hand rank after suit slipt
+
+	pHand := []int{pc1[0], pc2[0], pc3[0], pc4[0], pc5[0]}
+	pSuits := []int{pc1[1], pc2[1], pc3[1], pc4[1], pc5[1]}
+
+	sort.Ints(pHand)
+
+	/// Royal flush
+	if pHand[0] == 1 && pHand[1] == 10 && pHand[2] == 11 && pHand[3] == 12 && pHand[4] == 13 &&
+		pSuits[0] == pSuits[1] && pSuits[0] == pSuits[2] && pSuits[0] == pSuits[3] && pSuits[0] == pSuits[4] {
+
+		return 1
+
+	}
+
+	/// Straight flush
+	if pHand[0]+1 == pHand[1] && pHand[1]+1 == pHand[2] && pHand[2]+1 == pHand[3] && pHand[3]+1 == pHand[4] && pHand[0]+4 == pHand[4] &&
+		pSuits[0] == pSuits[1] && pSuits[0] == pSuits[2] && pSuits[0] == pSuits[3] && pSuits[0] == pSuits[4] {
+
+		return 2
+	}
+
+	/// Four of a Kind
+	if (pHand[0] == pHand[1] && pHand[1] == pHand[2] && pHand[2] == pHand[3]) ||
+		(pHand[1] == pHand[2] && pHand[2] == pHand[3] && pHand[3] == pHand[4]) {
+
+		return 3
+	}
+
+	/// Full House
+	if (pHand[0] == pHand[1] && pHand[1] == pHand[2] && pHand[3] == pHand[4]) ||
+		(pHand[0] == pHand[1] && pHand[2] == pHand[3] && pHand[3] == pHand[4]) {
+
+		return 4
+	}
+
+	/// Flush
+	if pSuits[0] == pSuits[1] && pSuits[0] == pSuits[2] && pSuits[0] == pSuits[3] && pSuits[0] == pSuits[4] {
+
+		return 5
+	}
+
+	/// Straight
+	if pHand[0]+1 == pHand[1] && pHand[1]+1 == pHand[2] && pHand[2]+1 == pHand[3] && pHand[3]+1 == pHand[4] && pHand[0]+4 == pHand[4] ||
+		pHand[0] == 1 && pHand[1] == 10 && pHand[2] == 11 && pHand[3] == 12 && pHand[4] == 13 {
+		return 6
+	}
+
+	/// Three of a Kind
+	if (pHand[0] == pHand[1] && pHand[1] == pHand[2]) ||
+		(pHand[1] == pHand[2] && pHand[2] == pHand[3]) ||
+		(pHand[2] == pHand[3] && pHand[3] == pHand[4]) {
+		return 7
+	}
+
+	/// Two Pair
+	if (pHand[0] == pHand[1] && pHand[2] == pHand[3]) ||
+		(pHand[1] == pHand[2] && pHand[3] == pHand[4]) ||
+		(pHand[0] == pHand[1] && pHand[3] == pHand[4]) {
+		return 8
+	}
+
+	/// Pair
+	if pHand[0] == pHand[1] || pHand[0] == pHand[2] || pHand[0] == pHand[3] || pHand[0] == pHand[4] ||
+		pHand[1] == pHand[2] || pHand[1] == pHand[3] || pHand[1] == pHand[4] ||
+		pHand[2] == pHand[3] || pHand[2] == pHand[4] ||
+		pHand[3] == pHand[4] {
+		return 9
+	} else {
+
+		return 10
+	}
+}
---
+++ main.go
@@ -0,0 +1,230 @@
+/*
+	dReam Tables Poker Hand Ranker
+	Originally written in C++ and used in dReam Tables Dero Poker Tables
+	https://dreamtables.net
+	Translated to go
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+var control int
+var confirm string
+var skip bool = true
+
+func handToText(rank int) string { /// Hand rank text
+	var handRankText string
+	switch rank {
+	case 1:
+		handRankText = "Royal Flush"
+	case 2:
+		handRankText = "Straight Flush"
+	case 3:
+		handRankText = "Four of a Kind"
+	case 4:
+		handRankText = "Full House"
+	case 5:
+		handRankText = "Flush"
+	case 6:
+		handRankText = "Straight"
+	case 7:
+		handRankText = "Three of a Kind"
+	case 8:
+		handRankText = "Two Pair"
+	case 9:
+		handRankText = "Pair"
+	case 10:
+		handRankText = "High Card"
+	}
+	return handRankText
+}
+
+func cardEquiv(card int) string { /// Card #s to unicode cards
+	var cardEquiv string
+	switch card {
+	case 1:
+		cardEquiv = "🂡"
+	case 2:
+		cardEquiv = "🂢"
+	case 3:
+		cardEquiv = "🂣"
+	case 4:
+		cardEquiv = "🂤"
+	case 5:
+		cardEquiv = "🂥"
+	case 6:
+		cardEquiv = "🂦"
+	case 7:
+		cardEquiv = "🂧"
+	case 8:
+		cardEquiv = "🂨"
+	case 9:
+		cardEquiv = "🂩"
+	case 10:
+		cardEquiv = "🂪"
+	case 11:
+		cardEquiv = "🂫"
+	case 12:
+		cardEquiv = "🂭"
+	case 13:
+		cardEquiv = "🂮"
+	case 14:
+		cardEquiv = "🂱"
+	case 15:
+		cardEquiv = "🂲"
+	case 16:
+		cardEquiv = "🂳"
+	case 17:
+		cardEquiv = "🂴"
+	case 18:
+		cardEquiv = "🂵"
+	case 19:
+		cardEquiv = "🂶"
+	case 20:
+		cardEquiv = "🂷"
+	case 21:
+		cardEquiv = "🂸"
+	case 22:
+		cardEquiv = "🂹"
+	case 23:
+		cardEquiv = "🂺"
+	case 24:
+		cardEquiv = "🂻"
+	case 25:
+		cardEquiv = "🂽"
+	case 26:
+		cardEquiv = "🂾"
+	case 27:
+		cardEquiv = "🃑"
+	case 28:
+		cardEquiv = "🃒"
+	case 29:
+		cardEquiv = "🃓"
+	case 30:
+		cardEquiv = "🃔"
+	case 31:
+		cardEquiv = "🃕"
+	case 32:
+		cardEquiv = "🃖"
+	case 33:
+		cardEquiv = "🃗"
+	case 34:
+		cardEquiv = "🃘"
+	case 35:
+		cardEquiv = "🃙"
+	case 36:
+		cardEquiv = "🃚"
+	case 37:
+		cardEquiv = "🃛"
+	case 38:
+		cardEquiv = "🃝"
+	case 39:
+		cardEquiv = "🃞"
+	case 40:
+		cardEquiv = "🃁"
+	case 41:
+		cardEquiv = "🃂"
+	case 42:
+		cardEquiv = "🃃"
+	case 43:
+		cardEquiv = "🃄"
+	case 44:
+		cardEquiv = "🃅"
+	case 45:
+		cardEquiv = "🃆"
+	case 46:
+		cardEquiv = "🃇"
+	case 47:
+		cardEquiv = "🃈"
+	case 48:
+		cardEquiv = "🃉"
+	case 49:
+		cardEquiv = "🃊"
+	case 50:
+		cardEquiv = "🃋"
+	case 51:
+		cardEquiv = "🃍"
+	case 52:
+		cardEquiv = "🃎"
+	}
+	return cardEquiv
+}
+
+func showMenu() { /// Start Menu
+	fmt.Println()
+	fmt.Println("  dReam Tables Poker Hand Ranker  ")
+	fmt.Println()
+	for {
+		fmt.Println("♠♥♣♦     Select Option #      ♦♣♥♠")
+		fmt.Println()
+		fmt.Println("1. Compare Hands")
+		fmt.Println("9. Exit")
+		fmt.Print("Option: ")
+		fmt.Scan(&control)
+		if control == 1 || control == 9 {
+			break
+		}
+		fmt.Println("Invalid Option.")
+		fmt.Println()
+	}
+}
+
+func another() {
+
+	if skip == false {
+		fmt.Println("Compare more hands? y/n")
+		for {
+			fmt.Scan(&confirm)
+
+			if confirm == "y" {
+				control = 1
+				break
+
+			} else if confirm == "n" {
+				control = 9
+				break
+			} else {
+				fmt.Println("Enter y / n")
+			}
+
+		}
+	}
+}
+
+func main() {
+	showMenu()
+	for {
+		another()
+		skip = false
+		if control == 9 { /// Check for exit
+			fmt.Print("Exiting...")
+			fmt.Println(" 🃖")
+			break
+		}
+		control = 0
+		clearHands()
+		for { /// User inputs how many hands
+			fmt.Print("How many hands to Compare: ")
+			fmt.Scan(&totalHands)
+			if totalHands > 1 && totalHands < 7 {
+				break
+			}
+			fmt.Println("Enter a Number between 2 and 6") /// If invalid
+		}
+
+		fmt.Println("\nCard values are 1 through 52, in suited order.")
+		fmt.Println("Spades = 1(A) to 13(K), Hearts = 14(A) to 26(K), \nClubs = 27(A) to 39(K), Diamonds = 40(A) to 52(K). \n\nExamples of input: ")
+		fmt.Println("Royal Flush (Spades) 1 13 12 11 10")
+		fmt.Println("Full House (Aces over two's) 1 14 27 2 15")
+		fmt.Println("Hand combinations can be input in any order")
+		fmt.Println()
+		getHands(totalHands)
+		fmt.Println()
+		compareAll()
+
+	}
+
+}
